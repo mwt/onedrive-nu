a0bab139fce47670ba0f2073ba2628d48df1a3b9
Matthew W. Thomas <matthew@mwt.me>
2023-04-22 16:37:11 -0500

    receive notification of changed files

--- .rclone.sh
+++ .rclone.sh
@@ -10,19 +10,26 @@ SYNC_DIR=$(dirname -- $0)
 ##################
 
 # Echo the date and time
-echo ">>>> Syncing OneDrive at" $(date +'[%D %T]') "<<<<"
+echo ">>>> Syncing OneDrive at $(date +'[%D %T]') <<<<"
 
 # Change to the directory where the script is located
 cd "$SYNC_DIR"
 
 # tell rclone to return error number 9 when nothing is copied
-rclone sync --error-on-no-transfer "$REMOTE_DIR" "./" --exclude-from ".rcloneignore"
+rclone sync --stats 0 --log-file="./.tmp" --log-level INFO --use-json-log --error-on-no-transfer "$REMOTE_DIR" "./" --exclude-from ".rcloneignore"
 RET_VAL=$?
 
 if [ $RET_VAL -eq 9 ]; then
     echo "nothing changed"
 elif [ $RET_VAL -eq 0 ]; then
     echo "$MY_XMPP $(date +'[%D %T]') files changed!" | smplxmpp
+    MODIFIED_FILES="$(jq -sr '[.[] | select(.object != null) | .msg + ": " + .object ] | join("\\\\n")' "./.tmp")"
+    echo "$MY_XMPP $MODIFIED_FILES" | smplxmpp
 else
     echo "$MY_XMPP $(date +'[%D %T]') Error! rclone exited with code $RET_VAL" | smplxmpp
+    # move the log file to a new file with the error code and timestamp
+    cp "./.tmp" "./.error-$RET_VAL-$(date +'[%D %T]').log"
 fi
+
+# Delete the log file
+rm "./.tmp"
---
+++ README.md
@@ -0,0 +1,7 @@
+# Sync folder and notify of changes
+
+This repository contains a script that will sync a folder to a remote and notify you of changes. The purpose of this is to get a push notification when files change on disk. This is useful for me because changes in this folder typically indicate that I have work to do.
+
+This script requires: shell, rclone, jq, and [smplxmpp](https://codeberg.org/tropf/smplxmpp).
+
+The script is intended to be run using cron.
